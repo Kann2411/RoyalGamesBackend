68be4bf74fbce511a5e73cd797a375ddccbde378
Kann2411 <kaan20031732@gmail.com>
2026-06-02 12:14:56 -0500

    Revert "configurando main.ts para verificar errores en render"
    This reverts commit 21bdd2f5a11e947e5387868a6cf7fbbaa269bb1a.

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ async function bootstrap() {
       accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
     });
     console.log('✅ MercadoPago configured');
-  } catch (error: any) {
+  } catch (error) {
     console.warn('⚠️ MercadoPago configuration warning:', error.message);
   }
 
