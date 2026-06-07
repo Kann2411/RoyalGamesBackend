a38cc8edc99ea803918586cb02f8501ceea9eaf2
Kann2411 <kaan20031732@gmail.com>
2026-06-06 22:29:48 -0500

    fix

--- src/modules/chips/dtos/chips-transaction.dto.ts
+++ src/modules/chips/dtos/chips-transaction.dto.ts
@@ -4,7 +4,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ChipsTransactionDto {
   @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000', description: 'User ID' })
   @IsUUID()
-  id: string;
+  userId: string;
 
   @ApiProperty({ example: 100, description: 'Chips amount' })
   @IsNumber()
