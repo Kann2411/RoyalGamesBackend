96f62efec181a9a3f6c6ce53098ce664f517c6f2
Kann2411 <kaan20031732@gmail.com>
2026-06-06 22:21:42 -0500

    cambio chipsDto

--- src/modules/chips/dtos/chips-transaction.dto.ts
+++ src/modules/chips/dtos/chips-transaction.dto.ts
@@ -4,10 +4,10 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ChipsTransactionDto {
   @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000', description: 'User ID' })
   @IsUUID()
-  userId: string;
+  id: string;
 
   @ApiProperty({ example: 100, description: 'Chips amount' })
   @IsNumber()
   @Min(1)
-  amount: number;
+  removeChip: number;
 }
