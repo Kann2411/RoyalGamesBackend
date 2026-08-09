9f2defe1f796f6a037abb73d8c9de15e4cf20950
royalgames51 <royalgames2025@gmail.com>
2026-08-08 23:31:27 -0300

    cors

--- src/config/cors-origins.ts
+++ src/config/cors-origins.ts
@@ -10,6 +10,7 @@ export const ALLOWED_ORIGINS = [
   'https://royalgames.lat',
   'https://minasroyal.s3.us-east-2.amazonaws.com',
   'https://baazaar.s3.us-east-2.amazonaws.com',
+  'https://bingoroyal.s3.us-east-2.amazonaws.com',
 ];
 
 export function isOriginAllowed(origin: string | undefined | null): boolean {
